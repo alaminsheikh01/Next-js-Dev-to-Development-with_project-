e7cf948a09c802771b4bd1304c2bd6a0fbea6f46
alamin <alamin@gmail.com>
2021-05-06 17:32:32 +0600

    add api in the index pages

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,21 @@
 import Link from "next/link";
-import Head from "next/head";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({ events }) {
+  console.log(events);
   return (
     <Layout>
-      <h1>Hello</h1>
-      <Link href="/about">About</Link>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
 }

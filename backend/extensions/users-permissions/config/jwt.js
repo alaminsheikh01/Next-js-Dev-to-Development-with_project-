8eb2f75522bbe23c3a65eefea83fc0c5490be694
alamin <alamin@gmail.com>
2021-05-08 08:50:12 +0600

    added strapi CMS  for backend

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3dcf231a-ae02-45c6-ba2c-574efb6540d1'
+};
--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -1,10 +1,48 @@
 import Layout from "../../components/Layout";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
 
 export default function EventSinglePage({ evt }) {
+  const deleteEvent = () => {
+    //
+  };
   return (
     <Layout>
-      <h3>{evt.name}</h3>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.data} at {evt.time}
+        </span>
+        <h3>{evt.name}</h3>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers: </h3>
+        <p>{evt.performers}</p>
+        <h3>Description: </h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>&larr; Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
